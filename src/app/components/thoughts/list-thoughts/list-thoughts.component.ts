f648551c94f42546db4ebd0049a9f4a3592cd1fb
Desenvolvimento 10 <desenvolvimento10@inforplus.local>
2023-08-17 13:20:45 -0300

    criada API json

--- src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
+++ src/app/components/thoughts/list-thoughts/list-thoughts.component.ts
@@ -7,22 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListThoughtsComponent implements OnInit {
   listThoughts = [
-    {
-      conteudo: 'Aprendendo Angular',
-      autoria: 'Dev',
-      modelo: 'modelo3',
-    },
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Angular',
-      modelo: 'modelo2',
-    },
-    {
-      conteudo:
-        'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
-      autoria: 'Lorem',
-      modelo: 'modelo1',
-    },
+    
   ];
 
   constructor() {}
